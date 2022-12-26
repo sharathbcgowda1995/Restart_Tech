6166a60196e1fe505d79a66ac9ed046e2214f6b7
Sharath C <sharathbctechnicalqueries@gmail.com>
2022-12-26 15:14:58 +0530

    substrction meth added

--- addition.py
+++ addition.py
@@ -2,6 +2,16 @@ class Addition :
     def add(self,a,b):
         return a+b;
 
+    def sub(self,c,d):
+        if(c<d):
+            print("enter the bigger number")
+        else:
+            return c-d;
+
 obj=Addition()
 result = obj.add(100,200)
 print(result)
+
+result = obj.sub(100,200)
+if (result is not None):
+    print(result)
